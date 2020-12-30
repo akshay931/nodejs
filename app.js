7e91d37ba1d4f30d4f884c3a9053dde9a6355d07
akshay931 <akshayb3129@gmail.com>
2020-12-30 13:03:58 +0530

    changed

--- app.js
+++ app.js
@@ -18,7 +18,10 @@ const io = require('socket.io')(server, {
 });
 
 io.on("connection", (socket) => {
-    console.log("New client connected");
+  
+    socket.on("update",(data)=>{console.log(data)});
+
+
     socket.on("disconnect", () => {
       console.log("Client disconnected");
     });
