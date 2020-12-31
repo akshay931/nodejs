3435267c386f5f0f676ac322ac3df539b90de1b1
akshay931 <akshayb3129@gmail.com>
2020-12-31 09:49:14 +0530

    changed

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 8080;
 //-------------------------------------------------------------------------
 app.use(cors());
 // app.use('/',index);
-app.use(express.static('out'));
+app.use(express.static('./out/'));
 
 const io = require('socket.io')(server, {
   cors: {
